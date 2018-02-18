25741e43d4ad3368fe8226d8a5cae69fd50e5973
Brian Cooley <coolmodev@gmail.com>
2018-02-18 11:23:27 -0600

    Remove debug statements

--- src/tools.cpp
+++ src/tools.cpp
@@ -23,20 +23,10 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
     return rmse;
   }
 
-  if (499 == estimations.size()) {
-    cout << "pxe, pye, re, rde, px, py, r, rd" << endl;
-  }
-
   for(unsigned int i=0; i < estimations.size(); ++i){
     VectorXd residual = estimations[i] - ground_truth[i];
     residual = residual.array()*residual.array();
     rmse += residual;
-
-    if (499 == estimations.size()) {
-      cout << estimations[i][0] << "," << estimations[i][1] << "," << estimations[i][2] << "," << estimations[i][3] << ",";
-      cout << ground_truth[i][0] << "," << ground_truth[i][1] << "," << ground_truth[i][2] << "," << ground_truth[i][3] << endl;
-    }
-
   }
 
   rmse = rmse/estimations.size();
